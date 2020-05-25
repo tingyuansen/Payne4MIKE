13d8e53dbbc9a829e7a31b16f0636b5dcb0809c7
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2020-05-25 09:58:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='Payne4MIKE',
-      version='0.1',
+      version='1.0',
       description='Tools for fitting Magellan/MIKE spectra with The Payne.',
       author='Yuan-Sen Ting',
       author_email='ting@ias.edu',
       license='MIT',
-      url='https://github.com/tingyuansen/The_Payne',
+      url='https://github.com/tingyuansen/Payne4MIKE',
       package_dir = {},
       packages=['Payne4MIKE'],
       package_data={'Payne4MIKE':['other_data/*.npz','other_data/*.fits']},
